d826b0fceb0b6ad7eba1327cbc85a333e34f105a
Ryan Moore <ryan.moore@greyrocksoft.com>
2013-02-02 15:57:42 -0700

    Adding initial rails framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeBrewSocial::Application.config.secret_token = '342ee82a7a3c360b9d18d4446bf83acf8361b490f36af1b948dfc0d268acc5dfc92889f417c4c7d5bc44c1f7fc57b37dad36bc26373694bce93f49cd468a2a17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HomeBrewSocial::Application.config.session_store :cookie_store, key: '_home_brew_social_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HomeBrewSocial::Application.config.session_store :active_record_store
